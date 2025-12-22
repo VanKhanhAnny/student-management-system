1ac4958862d6ca296ddb3e8ed90d8b172942e7d5
VanKhanhAnny <arianne.dangvankhanh@gmail.com>
2025-12-22 11:15:23 -0500

    student-service-docker-setup

--- student-service/src/main/java/com/codewithanny/studentservice/controller/StudentController.java
+++ student-service/src/main/java/com/codewithanny/studentservice/controller/StudentController.java
@@ -4,6 +4,8 @@ import com.codewithanny.studentservice.dto.CreateStudentValidationGroup;
 import com.codewithanny.studentservice.dto.StudentRequestDTO;
 import com.codewithanny.studentservice.dto.StudentResponseDTO;
 import com.codewithanny.studentservice.service.StudentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.groups.Default;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/students") //http://localhost:4000/students
+@Tag(name="Student", description = "API for managing Students")
 public class StudentController {
     private final StudentService studentService;
 
@@ -23,12 +26,14 @@ public class StudentController {
     }
 
     @GetMapping
+    @Operation(summary = "Get Students")
     public ResponseEntity<List<StudentResponseDTO>> getStudents() {
         List<StudentResponseDTO> students = studentService.getStudent();
         return ResponseEntity.ok().body(students);
     }
 
     @PostMapping
+    @Operation(summary = "Create a new Student")
     public ResponseEntity<StudentResponseDTO> createStudent(@Validated({Default.class, CreateStudentValidationGroup.class}) @RequestBody StudentRequestDTO studentRequestDTO) {
         StudentResponseDTO studentResponseDTO = studentService.createStudent(studentRequestDTO);
         return ResponseEntity.ok().body(studentResponseDTO);
@@ -36,6 +41,7 @@ public class StudentController {
 
     //localhost:4000/students/123445-1223455-12345
     @PutMapping("/{id}")
+    @Operation(summary = "Update a new Student")
     public ResponseEntity<StudentResponseDTO> updateStudent(@PathVariable UUID id, @Validated({Default.class}) @RequestBody StudentRequestDTO studentRequestDTO) {
         StudentResponseDTO studentResponseDTO = studentService.updateStudent(id, studentRequestDTO);
 
@@ -43,6 +49,7 @@ public class StudentController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete a Student")
     public ResponseEntity<Void> deleteStudent(@PathVariable UUID id) {
         studentService.deleteStudent(id);
         return ResponseEntity.noContent().build();
