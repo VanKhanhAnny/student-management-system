19d8125f404243c98656bfac84020435de64d639
VanKhanhAnny <arianne.dangvankhanh@gmail.com>
2025-12-24 15:10:25 -0500

    define-kafka-student-event-proto-file

--- student-service/src/main/java/com/codewithanny/studentservice/kafka/KafkaProducer.java
+++ student-service/src/main/java/com/codewithanny/studentservice/kafka/KafkaProducer.java
@@ -1,4 +1,13 @@
 package com.codewithanny.studentservice.kafka;
 
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
 public class KafkaProducer {
+    private final KafkaTemplate<String, byte[]> kafkaTemplate;
+
+    public KafkaProducer(KafkaTemplate<String, byte[]> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
 }
