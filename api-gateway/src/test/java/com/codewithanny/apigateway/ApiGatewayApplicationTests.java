fdd99d284962d6d6a886ae066cf55dd5cf38aed7
VanKhanhAnny <arianne.dangvankhanh@gmail.com>
2025-12-26 14:01:48 -0500

    api-gateway-setup-and-rules

---
+++ api-gateway/src/test/java/com/codewithanny/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithanny.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
