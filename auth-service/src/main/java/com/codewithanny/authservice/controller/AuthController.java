e292f9a926a603ad133eb05c65d69f15f7b675d7
VanKhanhAnny <arianne.dangvankhanh@gmail.com>
2025-12-27 15:25:24 -0500

    validate-token-logic

--- auth-service/src/main/java/com/codewithanny/authservice/controller/AuthController.java
+++ auth-service/src/main/java/com/codewithanny/authservice/controller/AuthController.java
@@ -7,11 +7,10 @@ import com.codewithanny.authservice.service.UserService;
 import com.codewithanny.authservice.util.JwtUtil;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -34,5 +33,20 @@ public class AuthController {
 
         String token = tokenOptional.get();
         return ResponseEntity.ok(new LoginResponseDTO(token));
+    }
+
+    @Operation(summary = "Validate Token")
+    @GetMapping("/validate")
+    public ResponseEntity<Void> validateToken(
+            @RequestHeader("Authorization") String authHeader) {
+
+        // Authorization: Bearer <token>
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        return authService.validateToken(authHeader.substring(7))
+                ? ResponseEntity.ok().build()
+                : ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 }
