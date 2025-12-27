b42e7bed56c66221d49dfcf6fc29e40e60a7ef6c
VanKhanhAnny <arianne.dangvankhanh@gmail.com>
2025-12-27 12:46:31 -0500

    auth-service-dockerise-run-deploy-seed-db

---
+++ auth-service/src/test/java/com/codewithanny/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithanny.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
