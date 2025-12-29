82276eb595a07a3f96a7a6ce27d7fc6dd92377f8
VanKhanhAnny <arianne.dangvankhanh@gmail.com>
2025-12-28 21:55:04 -0500

    create-ecs-cluster

--- infrastructure/src/main/java/com/codewithanny/stack/LocalStack.java
+++ infrastructure/src/main/java/com/codewithanny/stack/LocalStack.java
@@ -1,8 +1,11 @@
 package com.codewithanny.stack;
 
+
 import software.amazon.awscdk.*;
 import software.amazon.awscdk.services.ec2.*;
 import software.amazon.awscdk.services.ec2.InstanceType;
+import software.amazon.awscdk.services.ecs.CloudMapNamespaceOptions;
+import software.amazon.awscdk.services.ecs.Cluster;
 import software.amazon.awscdk.services.msk.CfnCluster;
 import software.amazon.awscdk.services.rds.*;
 import software.amazon.awscdk.services.route53.CfnHealthCheck;
@@ -11,6 +14,8 @@ import java.util.stream.Collectors;
 
 public class LocalStack extends Stack {
     private final Vpc vpc;
+    private final Cluster ecsCluster;
+
     public LocalStack(final App scope, final String id, final StackProps props) {
 
         super(scope, id, props);
@@ -23,6 +28,8 @@ public class LocalStack extends Stack {
         CfnHealthCheck studentDbHealthCheck = createDbHealthCheck(studentServiceDb, "StudentServiceDBHealthCheck");
 
         CfnCluster mskCluster = createMskCluster();
+
+        this.ecsCluster = createEcsCluster();
     }
 
     private Vpc createVpc() {
@@ -71,6 +78,16 @@ public class LocalStack extends Stack {
                                 .map(ISubnet::getSubnetId)
                                 .collect(Collectors.toList()))
                         .brokerAzDistribution("DEFAULT")
+                        .build())
+                .build();
+    }
+
+    // auth-service.student-management.local
+    private Cluster createEcsCluster() {
+        return Cluster.Builder.create(this, "StudentManagementCluster")
+                .vpc(vpc)
+                .defaultCloudMapNamespace(CloudMapNamespaceOptions.builder()
+                        .name("student-management.local")
                         .build())
                 .build();
     }
