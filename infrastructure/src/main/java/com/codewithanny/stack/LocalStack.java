d64d44b05d66743016a627dfac53662839e982f0
VanKhanhAnny <arianne.dangvankhanh@gmail.com>
2026-01-11 20:11:40 -0500

    update

--- infrastructure/src/main/java/com/codewithanny/stack/LocalStack.java
+++ infrastructure/src/main/java/com/codewithanny/stack/LocalStack.java
@@ -21,6 +21,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+
 public class LocalStack extends Stack {
     private final Vpc vpc;
     private final Cluster ecsCluster;
