d61341764f7ff08792026831e046e1d07ad03359
VanKhanhAnny <arianne.dangvankhanh@gmail.com>
2026-01-14 18:47:01 -0500

    cache-student-entity

---
+++ appointment-service/src/main/java/com/codewithanny/appointmentservice/entity/CachedStudent.java
@@ -0,0 +1,52 @@
+package com.codewithanny.appointmentservice.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name="cached_student")
+public class CachedStudent {
+
+    @Id
+    private UUID id;
+    private String fullName;
+    private String email;
+    private Instant updatedDate;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Instant getUpdatedDate() {
+        return updatedDate;
+    }
+
+    public void setUpdatedDate(Instant updatedDate) {
+        this.updatedDate = updatedDate;
+    }
+
+}
