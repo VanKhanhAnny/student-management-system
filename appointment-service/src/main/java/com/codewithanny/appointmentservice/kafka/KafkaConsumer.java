6543c848d72152478208a104f63f51e7981df396
VanKhanhAnny <arianne.dangvankhanh@gmail.com>
2026-01-15 19:54:09 -0500

    updated-cached-student-table

--- appointment-service/src/main/java/com/codewithanny/appointmentservice/kafka/KafkaConsumer.java
+++ appointment-service/src/main/java/com/codewithanny/appointmentservice/kafka/KafkaConsumer.java
@@ -1,6 +1,8 @@
 package com.codewithanny.appointmentservice.kafka;
 
 import com.codewithanny.appointmentservice.entity.Appointment;
+import com.codewithanny.appointmentservice.entity.CachedStudent;
+import com.codewithanny.appointmentservice.repository.CachedStudentRepository;
 import com.google.protobuf.InvalidProtocolBufferException;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
@@ -8,10 +10,18 @@ import student.events.StudentEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.Instant;
+import java.util.UUID;
+
 @Service
 public class KafkaConsumer {
 
     private static final Logger log = LoggerFactory.getLogger(KafkaConsumer.class);
+    private final CachedStudentRepository cachedstudentRepository;
+
+    public KafkaConsumer(CachedStudentRepository cachedstudentRepository) {
+        this.cachedstudentRepository = cachedstudentRepository;
+    }
 
     @KafkaListener(
             topics={"student.created", "student.updated"},
@@ -21,6 +31,14 @@ public class KafkaConsumer {
             StudentEvent studentEvent = StudentEvent.parseFrom(event);
 
             log.info("Received Student Event: {}", studentEvent.toString());
+
+            CachedStudent cachedStudent = new CachedStudent();
+            cachedStudent.setId(UUID.fromString(studentEvent.getStudentId()));
+            cachedStudent.setFullName(studentEvent.getName());
+            cachedStudent.setEmail(studentEvent.getEmail());
+            cachedStudent.setUpdatedDate(Instant.now());
+
+            cachedstudentRepository.save(cachedStudent);
         } catch (InvalidProtocolBufferException e) {
             log.error("Error deserializing Student Event: {}", e.getMessage());
         } catch (Exception e) {
