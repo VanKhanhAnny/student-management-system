c3bfd58124d74da51a49918878d5cc573168142b
VanKhanhAnny <arianne.dangvankhanh@gmail.com>
2026-01-15 19:33:02 -0500

    get-cached-students-details-and-add-to-dto

--- appointment-service/src/main/java/com/codewithanny/appointmentservice/service/AppointmentService.java
+++ appointment-service/src/main/java/com/codewithanny/appointmentservice/service/AppointmentService.java
@@ -1,7 +1,9 @@
 package com.codewithanny.appointmentservice.service;
 
 import com.codewithanny.appointmentservice.dto.AppointmentResponseDto;
+import com.codewithanny.appointmentservice.entity.CachedStudent;
 import com.codewithanny.appointmentservice.repository.AppointmentRepository;
+import com.codewithanny.appointmentservice.repository.CachedStudentRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -11,14 +13,22 @@ import java.util.List;
 public class AppointmentService {
 
     private final AppointmentRepository appointmentRepository;
+    private final CachedStudentRepository cachedStudentRepository;
 
-    public AppointmentService(AppointmentRepository appointmentRepository) {
+    public AppointmentService(AppointmentRepository appointmentRepository,  CachedStudentRepository cachedStudentRepository) {
         this.appointmentRepository = appointmentRepository;
+        this.cachedStudentRepository = cachedStudentRepository;
     }
 
     public List<AppointmentResponseDto> getAppointmentsByDateRange(LocalDateTime from, LocalDateTime to) {
         return appointmentRepository.findByStartTimeBetween(from, to).stream()
                 .map(appointment -> {
+
+                    String name = cachedStudentRepository
+                            .findById(appointment.getStudentId())
+                            .map(CachedStudent::getFullName)
+                            .orElse("Unknown");
+
                     AppointmentResponseDto appointmentResponseDto
                             = new AppointmentResponseDto();
 
@@ -28,6 +38,7 @@ public class AppointmentService {
                     appointmentResponseDto.setEndTime(appointment.getEndTime());
                     appointmentResponseDto.setReason(appointment.getReason());
                     appointmentResponseDto.setVersion(appointment.getVersion());
+                    appointmentResponseDto.setStudentName(name);
 
                     return appointmentResponseDto;
                 }).toList();
